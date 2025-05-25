7f6b1a5e0d8fee4cc505d39531c1b78377e2e89b
sloughmiller <sterlingloughmiller@u.boisestate.edu>
2025-05-25 12:47:35 -0600

    Updates for deploying in render

--- alembic/env.py
+++ alembic/env.py
@@ -77,11 +77,13 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
+    from sqlalchemy import create_engine
+
+    connectable = create_engine(
+        config.get_main_option("sqlalchemy.url"),
+        poolclass=pool.NullPool
     )
+
 
     with connectable.connect() as connection:
         context.configure(
