b4e2574b3bde56266c369e165d3783782fb2817b
sloughmiller <sterlingloughmiller@u.boisestate.edu>
2025-07-07 23:37:02 -0600

    updating shema init file for proper implementation of inventory and shared_inventory

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -4,3 +4,10 @@ from .condition import Condition, ConditionCreate, ConditionUpdate
 from .item import Item, ItemCreate, ItemUpdate
 from .location import Location, LocationCreate, LocationUpdate
 from .user import User, UserCreate, UserUpdate
+from .inventory import Inventory, InventoryCreate, InventoryUpdate
+from .shared_inventory import (
+    SharedInventoryBase,
+    SharedInventoryCreate,
+    SharedInventoryResponse,
+    SharedUserInfo,
+)
