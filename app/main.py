69b04c177a96a822afedf09125d0bc39c676e2af
sloughmiller <sterlingloughmiller@u.boisestate.edu>
2025-07-21 22:38:58 -0600

    testing middleware issues

--- app/main.py
+++ app/main.py
@@ -42,7 +42,7 @@ origins = [
 # ✅ Apply CORS middleware early
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
