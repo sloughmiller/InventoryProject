72c3d8cbcba3344a0d09748455c8b813a14766c4
sloughmiller <sterlingloughmiller@u.boisestate.edu>
2025-07-21 20:38:50 -0600

    Chasing refactor imports

--- frontend/src/pages/LocationsPage/LocationsForm.tsx
+++ frontend/src/pages/LocationsPage/LocationsForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import api from '../api/api';
-import { useSelectedInventory } from '../contexts/SelectedInventoryContext';
+import api from '../../api/api';
+import { useSelectedInventory } from '../../contexts/SelectedInventoryContext';
 
 interface LocationFormProps {
   onCreated: () => void;
